2905210ad541e4584c5e664a5e18326b17f31206
kahlstorf1 <dkahlstorf1@gmail.com>
2023-04-13 23:42:03 -0400

    Install and Configure Dependencies

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+// const pgp = require('pg-promise')
+// const db = pgp('postgres://username:password@host:port/database')
+
+// db.one('SELECT $1 AS value', 123)
+//   .then((data) => {
+//     console.log('DATA:', data.value)
+//   })
+//   .catch((error) => {
+//     console.log('ERROR:', error)
+//   })
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
