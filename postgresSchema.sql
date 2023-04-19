1ccb240e55fb6b96564e8d9a9df5fc153d2af0ad
kahlstorf1 <dkahlstorf1@gmail.com>
2023-04-18 22:44:58 -0400

    Updated Schema post-ETL

--- postgresSchema.sql
+++ postgresSchema.sql
@@ -4,52 +4,42 @@ CREATE TABLE IF NOT EXISTS aerio.overview (
   product_id INTEGER NOT NULL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   slogan VARCHAR(255) NOT NULL,
-  description VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
   category VARCHAR(255) NOT NULL,
-  default_price DECIMAL(6,2),
+  default_price FLOAT,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
- );
+);
 
-CREATE TYPE feature_type AS (
+CREATE TABLE IF NOT EXISTS aerio.features (
+  id SERIAL PRIMARY KEY,
+  product_id INTEGER REFERENCES aerio.overview(product_id),
   feature VARCHAR(255),
   value VARCHAR(255)
 );
 
-CREATE TABLE IF NOT EXISTS aerio.individual_product (
-  product_id INTEGER REFERENCES aerio.overview(product_id),
-  name VARCHAR(255) REFERENCES aerio.overview(name),
-  slogan VARCHAR(255) REFERENCES aerio.overview(slogan),
-  description VARCHAR(255) REFERENCES aerio.overview(description),
-  category VARCHAR(255) REFERENCES aerio.overview(category),
-  default_price INTEGER,
-  created_at TIMESTAMP REFERENCES aerio.overview(created_at),
-  updated_at TIMESTAMP REFERENCES aerio.overview(updated_at),
-  features feature_type[],
-  CONSTRAINT fk_individual_product_product_id
-    FOREIGN KEY (product_id)
-    REFERENCES aerio.overview(product_id)
- );
-
-CREATE TYPE photo_info AS (
-  thumbnail_url VARCHAR(255),
-  url VARCHAR(255)
+CREATE TABLE IF NOT EXISTS aerio.styles (
+ id SERIAL PRIMARY KEY,
+ product_id INTEGER REFERENCES aerio.overview(product_id),
+ name VARCHAR(255) NOT NULL,
+ sale_price INTEGER,
+ original_price INTEGER,
+ default_style INTEGER,
+ photos photo_info[5],
+ skus sku_info
 );
 
-CREATE TYPE sku_info AS (
-  quantity INTEGER,
-  size VARCHAR(255)
+CREATE TABLE IF NOT EXISTS aerio.skus (
+  id SERIAL PRIMARY KEY,
+  product_id INTEGER,
+  size VARCHAR(255),
+  quantity INTEGER
 );
 
- CREATE TABLE IF NOT EXISTS aerio.styles (
-  product_id INTEGER REFERENCES aerio.overview(product_id),
-  style_id INTEGER,
-  name VARCHAR(255) NOT NULL,
-  original_price INTEGER REFERENCES aerio.overview(default_price),
-  sale_price INTEGER,
-  photos photo_info[5],
-  skus sku_info,
-  CONSTRAINT fk_styles_product_id
-    FOREIGN KEY (product_id)
-    REFERENCES aerio.overview(product_id)
- );
+CREATE TABLE IF NOT EXISTS aerio.photos (
+  id SERIAL PRIMARY KEY,
+  product_id INTEGER,
+  url TEXT,
+  thumbnail_url TEXT
+);
+
