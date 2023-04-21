660697cecaba34ac1364456a2bd38159d8cf7d9d
kahlstorf1 <dkahlstorf1@gmail.com>
2023-04-20 22:31:14 -0400

    Build some routes

--- server.js
+++ server.js
@@ -23,17 +23,37 @@ db.connect((e) => {
 });
 
 app.get('/', (req, res) => {
-  const d1 = new Date();
-  const query = db.query('SELECT * FROM aerio.overview WHERE product_id = 71697;')
+  const query = 'SELECT aerio.styles.*, json_object_agg(size, quantity) AS skus FROM aerio.styles JOIN aerio.skus ON aerio.styles.product_id = aerio.skus.product_id WHERE aerio.styles.product_id = 1 GROUP BY aerio.styles.id;';
+  db.query(query)
+  .then((data) => {
+    res.send(data.rows);
+  });
+});
+
+app.get('/products/', (req, res) => {
+  const query = `SELECT * from aerio.overview LIMIT 5;`;
+  db.query(query)
     .then((data) => {
-      console.log(data.rows);
-      const d2 = new Date();
-      const diff = (d2.getTime() - d1.getTime());
-      console.log(diff);
+      res.send(data.rows);
     });
-  res.send('Hello World!');
+});
+
+app.get('/products/:product_id', (req, res) => {
+  const join = `SELECT product_id, json_object_agg(feature, value) AS features FROM aerio.features WHERE product_id = 71699 GROUP BY product_id`;
+  const query = `SELECT aerio.overview.*, features_agg.features FROM aerio.overview JOIN (${join}) AS features_agg ON aerio.overview.product_id = features_agg.product_id`;
+  db.query(query)
+    .then((data) => {
+      res.send(data.rows);
+    });
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+//mysql optimization ch8
+//summary of each idea
+//views "caching"
+//get rid of cnosole logs w morgan when stress testing
+
+//express middleware morgan for console logging
