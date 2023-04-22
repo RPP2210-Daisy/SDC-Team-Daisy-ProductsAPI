88ba8d12cb7d71281f71c954cfd8265ac3bfcf4a
kahlstorf1 <dkahlstorf1@gmail.com>
2023-04-22 15:08:24 -0400

    build initial DB test suite testing server/DB

--- test/server.test.js
+++ test/server.test.js
@@ -1,7 +1,32 @@
-const assert = require('chai').assert;
+const app = require('../server.js');
+const request = require('supertest');
+const chai = require('chai');
+const expect = chai.expect;
 
-const sum = (a, b) => a + b;
-
-it('1 + 1 should = 2', () => {
-  assert.equal(sum(1, 1), 2);
+describe('Products DB', () => {
+  it ('should get the first 5 rows of data from products from /products route', (done) => {
+    request(app)
+      .get('/products')
+      .end((err, res) => {
+        expect(res.body.length).to.equal(5);
+        expect(typeof res.body[0]).to.equal('object');
+        done();
+      });
+  });
+  it ('should get individual product with features from /products/:product_id route ', (done) => {
+    request(app)
+      .get('/products/71697')
+      .end((err, res) => {
+        console.log(res.body);
+        done();
+      });
+  });
+  it ('should get the first 5 rows of data from products', (done) => {
+    request(app)
+      .get('/products/71697/styles')
+      .end((err, res) => {
+        console.log(res.body);
+        done();
+      });
+  });
 });
