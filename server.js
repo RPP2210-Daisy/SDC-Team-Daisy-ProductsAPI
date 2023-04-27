b143f6f2d859186e9cb6a31f6fad84aa383428cc
kahlstorf1 <dkahlstorf1@gmail.com>
2023-04-27 10:10:40 -0400

    Rebuild queries to match shape of data from FEC API. Still tweaks to be made / Front End not yet workign

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const { Client } = require('pg');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static('./dist'));
+
 const db = new Client({
   user: process.env.PSQL_USER,
   host: 'localhost',
@@ -23,7 +25,7 @@ db.connect((e) => {
 });
 
 app.get('/products/', (req, res) => {
-  const query = 'SELECT * from aerio.overview LIMIT 5;';
+  const query = 'SELECT * FROM aerio.overview LIMIT 5;';
   db.query(query)
     .then((data) => {
       res.send(data.rows);
@@ -35,33 +37,60 @@ app.get('/products/:product_id', (req, res) => {
   SELECT aerio.overview.*, features_agg.features
   FROM aerio.overview
   JOIN (
-    SELECT product_id, json_object_agg(feature, value) AS features
+    SELECT product_id, json_agg(json_build_object('feature', feature, 'value', value)) AS features
     FROM aerio.features
-    WHERE product_id = 71697
+    WHERE product_id = ${req.params.product_id}
     GROUP BY product_id
   ) AS features_agg ON aerio.overview.product_id = features_agg.product_id;
   `;
+
   db.query(query)
     .then((data) => {
-      res.send(data.rows);
+      data ? res.send(data.rows[0]) : res.sendStatus(201);
     });
 });
 
 app.get('/products/:product_id/styles', (req, res) => {
   const query = `
-  SELECT aerio.styles.*,
-    json_object_agg(size, quantity) AS skus,
-    (SELECT json_agg(json_build_object('url', url, 'thumbnail_url', thumbnail_url))
-      FROM aerio.photos
-      WHERE aerio.photos.style_id = aerio.styles.id) AS photos
-  FROM aerio.styles
-  JOIN aerio.skus ON aerio.styles.id = aerio.skus.style_id
-  WHERE aerio.styles.product_id = 71699
-  GROUP BY aerio.styles.id;
-`;
+  WITH styles_agg AS (
+    SELECT
+      aerio.styles.product_id,
+      json_agg(
+        json_build_object(
+          'id', aerio.styles.id,
+          'name', aerio.styles.name,
+          'sale_price', aerio.styles.sale_price,
+          'original_price', aerio.styles.original_price,
+          'default_style', aerio.styles.default_style,
+          'photos', (
+            SELECT json_agg(json_build_object('url', url, 'thumbnail_url', thumbnail_url))
+            FROM aerio.photos
+            WHERE aerio.photos.style_id = aerio.styles.id
+          ),
+          'skus', (
+            SELECT json_object_agg(skus.id, json_build_object('size', size, 'quantity', quantity))
+            FROM (
+              SELECT id, size, quantity
+              FROM aerio.skus
+              WHERE aerio.skus.style_id = aerio.styles.id
+              LIMIT 6
+            ) AS skus
+          )
+        )
+      ) AS styles
+    FROM aerio.styles
+    WHERE aerio.styles.product_id = ${req.params.product_id}
+    GROUP BY aerio.styles.product_id
+  )
+  SELECT
+    product_id,
+    json_array_elements(styles) AS results
+  FROM styles_agg;
+  `;
+
   db.query(query)
     .then((data) => {
-      res.send(data.rows);
+      res.send(data.rows[0]);
     });
 });
 
