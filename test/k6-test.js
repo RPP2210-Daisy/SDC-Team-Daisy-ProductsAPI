5c2862b0addc7b4b27b04b24dc61088d20d3f896
kahlstorf1 <dkahlstorf1@gmail.com>
2023-05-11 23:15:06 -0400

    Latest k6 with user ramp

--- test/k6-test.js
+++ test/k6-test.js
@@ -2,10 +2,12 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 import { Counter, Trend } from 'k6/metrics';
 
-const r1Latency = new Trend('R1 Latency');
-const r2Latency = new Trend('R2 Latency');
-const r3Latency = new Trend('R3 Latency');
-const r4Latency = new Trend('R4 Latency');
+const latencyMetrics = {
+  r1Latency: new Trend('R1 Latency'),
+  r2Latency: new Trend('R2 Latency'),
+  r3Latency: new Trend('R3 Latency'),
+  r4Latency: new Trend('R4 Latency'),
+};
 
 const errorRate = new Counter('Error Rate');
 
@@ -20,8 +22,11 @@ function checkStatus(res, routeName) {
 }
 
 export const options = {
-  vus: 500,
-  duration: '30s',
+  stages: [
+    { duration: '20s', target: 1000 },
+    { duration: '20s', target: 2000 },
+    { duration: '20s', target: 3000 },
+  ],
 };
 
 export default function () {
@@ -32,19 +37,6 @@ export default function () {
   const res3 = http.get(`http://localhost:3000/products/${prodID}/styles`);
   const res4 = http.get(`http://localhost:3000/products/${prodID}/related`);
 
-  check(res1, {
-    'route1 status is 200': (r) => r.status === 200,
-  });
-  check(res2, {
-    'route2 status is 200': (r) => r.status === 200,
-  });
-  check(res3, {
-    'route3 status is 200': (r) => r.status === 200,
-  });
-  check(res4, {
-    'route4 status is 200': (r) => r.status === 200,
-  });
-
   // Check status and track error rate
   checkStatus(res1, 'route1');
   checkStatus(res2, 'route2');
@@ -52,10 +44,10 @@ export default function () {
   checkStatus(res4, 'route4');
 
   // Track query latency
-  r1Latency.add(res1.timings.waiting);
-  r2Latency.add(res2.timings.waiting);
-  r3Latency.add(res3.timings.waiting);
-  r4Latency.add(res4.timings.waiting);
+  latencyMetrics.r1Latency.add(res1.timings.waiting);
+  latencyMetrics.r2Latency.add(res2.timings.waiting);
+  latencyMetrics.r3Latency.add(res3.timings.waiting);
+  latencyMetrics.r4Latency.add(res4.timings.waiting);
 
   sleep(1);
 }
