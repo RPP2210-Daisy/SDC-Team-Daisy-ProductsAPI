df329b7d344788ac44c2f4942d449a0ff3147099
kahlstorf1 <dkahlstorf1@gmail.com>
2023-05-11 23:14:26 -0400

    Complete adding Redis to all routes

--- server.js
+++ server.js
@@ -18,18 +18,9 @@ const db = new Pool({
 });
 
 const redis = new createClient();
-redis.connect();
-redis.on('ready', () => {
-  console.log('Redis client connected');
-});
 
-db.connect((e) => {
-  if (e) {
-    console.log(e);
-  } else {
-    console.log(`Connected to PSQL DB: ${process.env.PSQL_DB}`);
-  }
-});
+redis.connect();
+db.connect();
 
 app.get('/products', async (req, res) => {
   try {
@@ -53,20 +44,32 @@ app.get('/products/:product_id', async (req, res) => {
     let productID;
     typeof req.params.product_id === 'string' ? productID = 71697 : productID = req.params.product_id;
 
-    const query = `
-      SELECT aerio.overview.*, features_agg.features
-      FROM aerio.overview
-      JOIN (
-        SELECT product_id, json_agg(json_build_object('feature', feature, 'value', value)) AS features
-        FROM aerio.features
-        WHERE product_id = ${productID}
-        GROUP BY product_id
-      ) AS features_agg ON aerio.overview.product_id = features_agg.product_id;
-      `;
-    const data = await db.query(query);
+    const cacheKey = `product_overview_${productID}`;
+    let data = await redis.get(cacheKey);
+
+    if (!data) {
+      try {
+        const query = `
+          SELECT aerio.overview.*, features_agg.features
+          FROM aerio.overview
+          JOIN (
+            SELECT product_id, json_agg(json_build_object('feature', feature, 'value', value)) AS features
+            FROM aerio.features
+            WHERE product_id = ${productID}
+            GROUP BY product_id
+          ) AS features_agg ON aerio.overview.product_id = features_agg.product_id;
+          `;
+        data = await db.query(query);
+        redis.set(cacheKey, JSON.stringify(data));
+      } catch (dbError) {
+        return res.status(500).send('Error querying the database');
+      }
+    } else {
+      data = JSON.parse(data);
+    }
     res.send(data.rows[0]);
-  } catch (e) {
-    res.status(500).send('Server Error from /products/:product_id');
+  } catch (err) {
+    res.status(500).send('Error retrieving data from cache');
   }
 });
 
@@ -75,51 +78,59 @@ app.get('/products/:product_id/styles', async (req, res) => {
     let productID;
     typeof req.params.product_id === 'string' ? productID = 71697 : productID = req.params.product_id;
 
-    const query = `
-    WITH styles_agg AS (
-      SELECT
-        aerio.styles.product_id,
-        json_agg(
-          json_build_object(
-            'id', aerio.styles.id,
-            'name', aerio.styles.name,
-            'sale_price', aerio.styles.sale_price,
-            'original_price', aerio.styles.original_price,
-            'default_style', aerio.styles.default_style,
-            'photos', (
-              SELECT json_agg(json_build_object('url', url, 'thumbnail_url', thumbnail_url))
-              FROM aerio.photos
-              WHERE aerio.photos.style_id = aerio.styles.id
-            ),
-            'skus', (
-              SELECT json_object_agg(skus.id, json_build_object('size', size, 'quantity', quantity))
-              FROM (
-                SELECT id, size, quantity
-                FROM aerio.skus
-                WHERE aerio.skus.style_id = aerio.styles.id
-                LIMIT 6
-              ) AS skus
+    const cacheKey = `product_${productID}_styles`;
+    let data = await redis.get(cacheKey);
+
+    if (!data) {
+      const query = `
+      WITH styles_agg AS (
+        SELECT
+          aerio.styles.product_id,
+          json_agg(
+            json_build_object(
+              'id', aerio.styles.id,
+              'name', aerio.styles.name,
+              'sale_price', aerio.styles.sale_price,
+              'original_price', aerio.styles.original_price,
+              'default_style', aerio.styles.default_style,
+              'photos', (
+                SELECT json_agg(json_build_object('url', url, 'thumbnail_url', thumbnail_url))
+                FROM aerio.photos
+                WHERE aerio.photos.style_id = aerio.styles.id
+              ),
+              'skus', (
+                SELECT json_object_agg(skus.id, json_build_object('size', size, 'quantity', quantity))
+                FROM (
+                  SELECT id, size, quantity
+                  FROM aerio.skus
+                  WHERE aerio.skus.style_id = aerio.styles.id
+                  LIMIT 6
+                ) AS skus
+              )
             )
-          )
-        ) AS styles
-      FROM aerio.styles
-      WHERE aerio.styles.product_id = ${productID}
-      GROUP BY aerio.styles.product_id
-    ),
-    results_array AS (
+          ) AS styles
+        FROM aerio.styles
+        WHERE aerio.styles.product_id = ${productID}
+        GROUP BY aerio.styles.product_id
+      ),
+      results_array AS (
+        SELECT
+          product_id,
+          json_array_elements(styles) AS result
+        FROM styles_agg
+      )
       SELECT
         product_id,
-        json_array_elements(styles) AS result
-      FROM styles_agg
-    )
-    SELECT
-      product_id,
-      json_agg(result) AS results
-    FROM results_array
-    GROUP BY product_id;
-    `;
+        json_agg(result) AS results
+      FROM results_array
+      GROUP BY product_id;
+      `;
+      data = await db.query(query);
+      redis.set(cacheKey, JSON.stringify(data));
+    } else {
+      data = JSON.parse(data);
+    }
 
-    const data = await db.query(query);
     res.send(data.rows[0]);
   } catch (e) {
     res.status(500).send('Server Error from /products/:product_id/styles');
@@ -131,20 +142,26 @@ app.get('/products/:product_id/related', async (req, res) => {
     let productID;
     typeof req.params.product_id === 'string' ? productID = 71697 : productID = req.params.product_id;
 
-    const query = `
-    SELECT ARRAY_AGG(related_product_id)
-    FROM aerio.related
-    WHERE product_id = ${productID};
-  `;
-    const data = await db.query(query);
+    const cacheKey = `product_${productID}_related`;
+    let data = await redis.get(cacheKey);
+
+    if (!data) {
+      const query = `
+      SELECT ARRAY_AGG(related_product_id)
+      FROM aerio.related
+      WHERE product_id = ${productID};
+    `;
+      data = await db.query(query);
+      redis.set(cacheKey, JSON.stringify(data), 'EX', 3600);
+    } else {
+      data = JSON.parse(data);
+    }
     res.send(data.rows[0].array_agg);
   } catch (e) {
     res.status(500).send('Server Error from /products/:product_id/related');
   }
 });
 
-app.listen(port, () => {
-  console.log(`SDC-Products listening on port ${port}`);
-});
+app.listen(port);
 
 module.exports = app;
